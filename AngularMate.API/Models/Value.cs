d389f21558b842fdc889229865d5614995b6c9b5
David Garza <dgarza0413@gmail.com>
2020-07-19 17:05:32 -0500

    added models and datacontext as a service in startup cs file

---
+++ AngularMate.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace AngularMate.API.Models
+{
+    public class Value
+    {
+        // used prop to auto gen
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
